db67320589579e4c8a00731acf381b30f28b4f0b
prashantg722 <95338520+prashantg722@users.noreply.github.com>
2022-01-07 11:47:09 +0530

    message added

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ class Test {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
 System.out.println("Hello, Good morning!");
-System.out.println("Hello ");
+System.out.println("Hello, Have a nice day");
 
     }
 }
